203e224ea795eaf927ce0fdcd919dd34f923cecb
King <motokingjr@sbcglobal.net>
2023-09-03 12:12:34 -0400

    Separated code between Lib and Tests

---
+++ lib/GradeConverter.dart
@@ -0,0 +1,16 @@
+class GradeConverter {
+  final Map<int, String> gradeLibrary = {
+    90: 'A',
+    80: 'B',
+    70: 'C',
+    60: 'D',
+    0: 'F',
+  };
+
+  String convertGrade(int numericGrade) {
+    return gradeLibrary.entries.firstWhere(
+          (entry) => numericGrade >= entry.key,
+      orElse: () => const MapEntry(0, 'F'),
+    ).value;
+  }
+}
--- test/grade_tool_test.dart
+++ test/grade_tool_test.dart
@@ -1,37 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:gradetool/GradeConverter.dart';
+
 
 void main() {
-  final gradeLibrary = {
-    90: 'A',
-    80: 'B',
-    70: 'C',
-    60: 'D',
-    0: 'F',
-  };
-
-  test('Grade Conversion Test', () {
-    const numericGrade = 90;
-    const expectedLetterGrade = 'A';
-
-    final actualLetterGrade = gradeLibrary[numericGrade];
-
-    expect(actualLetterGrade, expectedLetterGrade);
-  }); //End Test for A
-
-  test('Grade Conversion Test', () {
-    const numericGrade = 80;
-    const expectedLetterGrade = 'B';
-
-    final actualLetterGrade = gradeLibrary[numericGrade];
-
-    expect(actualLetterGrade, expectedLetterGrade);
-  }); //End test for B
+  GradeConverter converter = GradeConverter();
 
   test('Grade 91 should be an A', () {
     const numericGrade = 91;
     const expectedLetterGrade = 'A';
-
-    final converter = GradeConverter();
 
     final actualLetterGrade = converter.convertGrade(numericGrade);
 
@@ -41,19 +17,4 @@ void main() {
 
 }
 
-class GradeConverter {
-  final Map<int, String> gradeLibrary = {
-    90: 'A',
-    80: 'B',
-    70: 'C',
-    60: 'D',
-    0: 'F',
-  };
 
-  String convertGrade(int numericGrade) {
-    return gradeLibrary.entries.firstWhere(
-          (entry) => numericGrade >= entry.key,
-      orElse: () => MapEntry(0, 'F'),
-    ).value;
-  }
-}
