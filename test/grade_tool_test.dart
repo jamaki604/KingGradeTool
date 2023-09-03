5e79fa9f301993ff4674329b358fb996a336689d
King <motokingjr@sbcglobal.net>
2023-09-03 11:28:06 -0400

    Updated Dictionary and added second test for B

--- test/grade_tool_test.dart
+++ test/grade_tool_test.dart
@@ -6,6 +6,10 @@ void main(){
 
   final gradeLibrary = {
     90: 'A',
+    80: 'B',
+    70: 'C',
+    60: 'D',
+    0: 'F',
   };
 
   test('Grade Conversion Test', () {
@@ -16,5 +20,14 @@ void main(){
 
     expect(actualLetterGrade, expectedLetterGrade);
   }); //End Test for A
+
+  test('Grade Conversion Test', () {
+    const numericGrade = 80;
+    const expectedLetterGrade = 'B';
+
+    final actualLetterGrade = gradeLibrary[numericGrade];
+
+    expect(actualLetterGrade, expectedLetterGrade);
+  });//End test for B
 
 } //End Main
