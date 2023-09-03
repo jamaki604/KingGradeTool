b35238514453ff189a797436fd80afc2409b5cb6
King <motokingjr@sbcglobal.net>
2023-09-03 12:09:21 -0400

    refactored greater then test, added class to call GradeConverter with method to convert grade

--- test/grade_tool_test.dart
+++ test/grade_tool_test.dart
@@ -1,8 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-void main(){
-
-
+void main() {
   final gradeLibrary = {
     90: 'A',
     80: 'B',
@@ -27,53 +25,35 @@ void main(){
     final actualLetterGrade = gradeLibrary[numericGrade];
 
     expect(actualLetterGrade, expectedLetterGrade);
-  });//End test for B
+  }); //End test for B
 
   test('Grade 91 should be an A', () {
     const numericGrade = 91;
     const expectedLetterGrade = 'A';
 
-    final actualLetterGrade = gradeLibrary.entries.firstWhere(
-            (entry) => numericGrade >= entry.key,
-        orElse: () => const MapEntry(0, 'F')).value;
+    final converter = GradeConverter();
+
+    final actualLetterGrade = converter.convertGrade(numericGrade);
 
     expect(actualLetterGrade, equals(expectedLetterGrade));
-  });//End Test for Greater then Value for A being 91
+  }); //End Test for Greater then Value for A being 91
 
-  test('Grade 101 should be an A', () {
-    const numericGrade = 101;
-    const expectedLetterGrade = 'A';
 
-    final actualLetterGrade = gradeLibrary.entries.firstWhere(
-            (entry) => numericGrade >= entry.key,
-        orElse: () => const MapEntry(0, 'F')).value;
+}
 
-    expect(actualLetterGrade, equals(expectedLetterGrade));
+class GradeConverter {
+  final Map<int, String> gradeLibrary = {
+    90: 'A',
+    80: 'B',
+    70: 'C',
+    60: 'D',
+    0: 'F',
+  };
 
-  });//End Test for 101
-
-  test('Grade 71 should be an C', () {
-    const numericGrade = 71;
-    const expectedLetterGrade = 'C';
-
-    final actualLetterGrade = gradeLibrary.entries.firstWhere(
-            (entry) => numericGrade >= entry.key,
-        orElse: () => const MapEntry(0, 'F')).value;
-
-    expect(actualLetterGrade, equals(expectedLetterGrade));
-
-  });//End test for C
-
-  test('Grade 0 should be an F', () {
-    const numericGrade = 0;
-    const expectedLetterGrade = 'F';
-
-    final actualLetterGrade = gradeLibrary.entries.firstWhere(
-            (entry) => numericGrade >= entry.key,
-        orElse: () => const MapEntry(0, 'F')).value;
-
-    expect(actualLetterGrade, equals(expectedLetterGrade));
-
-  });//End test for F
-
-} //End Main
+  String convertGrade(int numericGrade) {
+    return gradeLibrary.entries.firstWhere(
+          (entry) => numericGrade >= entry.key,
+      orElse: () => MapEntry(0, 'F'),
+    ).value;
+  }
+}
