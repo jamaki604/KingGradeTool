8c7921df492b2b69628e1631eeef5b41c6a8308f
King <motokingjr@sbcglobal.net>
2023-09-18 16:11:03 -0400

    Re-added additional tests that were mistakenly removed under Clean Code Assumption, Updated Dart convention, Removed noise comment.

--- test/grade_tool_test.dart
+++ test/grade_tool_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:gradetool/GradeConverter.dart';
-
+import 'package:gradetool/grade_converter.dart';
 
 void main() {
   GradeConverter converter = GradeConverter();
@@ -12,9 +11,59 @@ void main() {
     final actualLetterGrade = converter.convertGrade(numericGrade);
 
     expect(actualLetterGrade, equals(expectedLetterGrade));
-  }); //End Test for Greater then Value for A being 91
+  });
 
+  test('Grade 101 should be an A', () {
+    const numericGrade = 101;
+    const expectedLetterGrade = 'A';
 
+    final actualLetterGrade = converter.convertGrade(numericGrade);
+
+    expect(actualLetterGrade, equals(expectedLetterGrade));
+  });
+
+  test('Grade 82 should be an B', () {
+    const numericGrade = 82;
+    const expectedLetterGrade = 'B';
+
+    final actualLetterGrade = converter.convertGrade(numericGrade);
+
+    expect(actualLetterGrade, equals(expectedLetterGrade));
+  });
+
+  test('Grade 73 should be an C', () {
+    const numericGrade = 73;
+    const expectedLetterGrade = 'C';
+
+    final actualLetterGrade = converter.convertGrade(numericGrade);
+
+    expect(actualLetterGrade, equals(expectedLetterGrade));
+  });
+
+  test('Grade 64 should be an D', () {
+    const numericGrade = 64;
+    const expectedLetterGrade = 'D';
+
+    final actualLetterGrade = converter.convertGrade(numericGrade);
+
+    expect(actualLetterGrade, equals(expectedLetterGrade));
+  });
+
+  test('Grade 55 should be an F', () {
+    const numericGrade = 55;
+    const expectedLetterGrade = 'F';
+
+    final actualLetterGrade = converter.convertGrade(numericGrade);
+
+    expect(actualLetterGrade, equals(expectedLetterGrade));
+  });
+
+  test('Grade 0 should be an F', () {
+    const numericGrade = 0;
+    const expectedLetterGrade = 'F';
+
+    final actualLetterGrade = converter.convertGrade(numericGrade);
+
+    expect(actualLetterGrade, equals(expectedLetterGrade));
+  });
 }
-
-
